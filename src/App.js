1e847c556d028e24e47dc93d737fee039231886d
Illian Yamandú <yamandu.developer@gmail.com>
2022-09-06 21:44:10 -0300

    Pegando dados da API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useState } from 'react';
+import Tmdb from './Tmdb';
 
 export default () =>{
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(()=>{
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+
+      setMovieList(list);
+    }
+
+    loadAll();
+  },[])
+
+
   return (
+
     <div>
       olá mundo
     </div>
-  )
+  );
 }
