bcc1989f58474fe26bcb9330423b0b642bfb0aed
Illian Yamandú <yamandu.developer@gmail.com>
2022-09-07 19:17:58 -0300

    detalhes da lista de filmes

--- src/App.js
+++ src/App.js
@@ -69,6 +69,12 @@ export default () =>{
         Direitos de imagem para Netiflix <br />
         Dados pego do site themoviedb.org
       </footer>
+        
+      {movieList <= 0 &&
+        <div className='loading'>
+            <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando" />
+        </div>
+      } 
     </div>
   );
 }
